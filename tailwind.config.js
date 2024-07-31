ced145885efff9cdbe4bb8006d74192740621508
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-07-31 20:54:02 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./*.html"
+    ],
+    theme: {
+      colors:{
+        'primary' : '#010104',
+        'secundary' : '#020024',
+        'dblue' : '#0600C2',
+        'lblue' : '#3A31D8',
+        'light' : '#EAE9FC',
+        'white' : '#FFFFFF',
+        'secbg' : '#04040A',
+        'red' : '#B42318',
+      },  
+    },
+  };
