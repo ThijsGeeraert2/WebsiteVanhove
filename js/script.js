c9d244aab11613c8696ad12a53a2121c0b1f6872
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-09-21 17:01:51 +0200

    upd projecten

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ const menu = document.getElementById('menu')
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 });
+
+function changeImg(){
+    console.log(document.getElementById("toChange1").src)
+    console.log("help")
+
+    if (document.getElementById("toChange1").src == "../images/huis.jpg"){
+        document.getElementById("toChange1").src = "./images/huiswerken.jpg"
+
+    }
+}
